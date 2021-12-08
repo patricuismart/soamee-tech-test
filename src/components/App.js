739ccbf7e3b271eb88c8d555ccf38aca0d5e7709
patricuismart <patricuismart@gmail.com>
2021-12-08 11:26:23 +0100

    Add main structure

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// File src/components/App.js
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div>
+      <h1 class="uno">Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
