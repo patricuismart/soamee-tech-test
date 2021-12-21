dd6125cefaaa2d8049dc6b7d1f7b1db9fc13f888
patricuismart <patricuismart@gmail.com>
2021-12-21 19:40:36 +0100

    Fix UserList component

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,19 +1,16 @@
 import UserItem from './UserItem';
 
-const UserList = (props) => {
-  const html = props.data.map((userData, index) => (
-    <>
-      <li key={index} className="card__user">
-        <UserItem userData={userData} />
-      </li>
-    </>
-  ));
+const UserList = (props) => (
+  <>
+    <h1 className="card__title">Resultados</h1>
+    <ul className="card__list">
+      {props.data.map((userData, index) => (
+        <li key={index} className="card__user">
+          <UserItem userData={userData} handleErase={props.handleErase} />
+        </li>
+      ))}
+    </ul>
+  </>
+);
 
-  return (
-    <>
-      <h1 className="card__title">Resultados</h1>
-      <ul className="card__list">{html}</ul>
-    </>
-  );
-};
 export default UserList;
