8c53aa4954cf1956f83e991908ab80292116a4c6
patricuismart <patricuismart@gmail.com>
2021-12-21 19:07:26 +0100

    Fix fetch by id

--- src/services/DataApi.js
+++ src/services/DataApi.js
@@ -1,22 +1,25 @@
-function getDataFromApi() {
-  for (let index = 0; index < 25; index++) {
-    return fetch(`https://jsonplaceholder.typicode.com/users`)
-      .then((response) => response.json())
-      .then((json) => {
-        //Clean data recived from Api with a map
-        return json.map((user) => {
-          //console.log('Fetch', user);
-          return {
-            id: user.id,
-            name: user.name,
-            email: user.email,
-            city: user.address.city,
-            company: user.company.name,
-            website: user.website,
-          };
-        });
+function getDataFromApi(userId) {
+  //fetch by user ID
+
+  const apiUrl = `https://jsonplaceholder.typicode.com/users${
+    userId ? '?id=' + userId : ''
+  }`;
+  return fetch(apiUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      //Clean data recived from Api with a map
+      return json.map((user) => {
+        //console.log('Fetch', user);
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          city: user.address.city,
+          company: user.company.name,
+          website: user.website,
+        };
       });
-  }
+    });
 }
 
 export default getDataFromApi;
